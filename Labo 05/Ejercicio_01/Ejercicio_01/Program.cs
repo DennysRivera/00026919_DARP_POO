660a0802a6cc0f12a003590d72b5420d1b7f542e
Dennys Rivera <00026919@uca.edu.sv>
2020-04-25 00:16:15 -0600

    Add: Lab 05 Exercise 01

---
+++ Labo 05/Ejercicio_01/Ejercicio_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0, n;
+            
+            Console.Write("Ingrese cuantos elementos de la serie de Fibonacci mostrar: ");
+            n = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Estos son los primeros " + n + " elementos:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a + " ");
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
