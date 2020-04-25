a15701e352b20f16d782c6929a824da7610cfd0c
Dennys Rivera <00026919@uca.edu.sv>
2020-04-25 00:17:57 -0600

    Add: Lab 05 Exercise 05

---
+++ Labo 05/Ejercicio_05/Ejercicio_05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio_05
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Escriba algo: ");
+            String frase = Console.ReadLine();
+            
+            List<char> vocales = new List<char>();
+            char letra;
+            foreach (var l in frase)
+            {
+                letra = l;
+                switch (letra)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                    case 'A':
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U': vocales.Add(letra); break;
+                }
+            }
+            Console.WriteLine("Las vocales encontradas son: ");
+            foreach (var vocal in vocales)
+            {
+                Console.Write(vocal + " ");
+            }
+        }
+    }
+}
