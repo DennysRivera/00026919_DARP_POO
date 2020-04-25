b514f495eedc90900ce45f2730b530b69551ff95
Dennys Rivera <00026919@uca.edu.sv>
2020-04-25 00:17:07 -0600

    Add: Lab 05 Exercise 02

---
+++ Labo 05/Ejercicio_02/Ejercicio_02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ejercicio_02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool palin = true;
+            
+            Console.Write("Ingrese una frase: ");
+            String frase = Console.ReadLine(), fraseInversa = " ";
+
+            if (frase != null)
+            {
+                frase = frase.Trim();
+
+                for (int i = 0; i < frase.Length; i++)
+                {
+                    fraseInversa += frase[frase.Length - 1 - i];
+                }
+
+                if (frase.ToLower().Replace(" ","").Equals(fraseInversa.ToLower().Replace(" ", "")))
+                {
+                    Console.WriteLine("La palabra/frase es palindromo: " + palin);
+                }
+                else
+                {
+                    Console.WriteLine("La palabra frase es palindromo: " + !palin);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No había palabra que verificar");
+            }
+        }
+    }
+}
