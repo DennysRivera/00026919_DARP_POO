40bc6429f08e58b384d7d5a74e4716c972037c24
Dennys Rivera <00026919@uca.edu.sv>
2020-04-25 00:17:43 -0600

    Add: Lab 05 Exercise 04

---
+++ Labo 05/Ejercicio_04/Ejercicio_04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Ejercicio_04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese una cadena de texto: ");
+            String frase = Console.ReadLine();
+            frase = frase.Trim();
+            if (frase != "")
+            {
+                Console.WriteLine("La cadena de texto modificada es:\n" + frase.Replace('a', 'b'));   
+            }
+            else
+            {
+                Console.WriteLine("No había cadena de texto");
+            }
+        }
+    }
+}
