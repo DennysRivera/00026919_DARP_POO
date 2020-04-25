c11af518524cf52aa8e291fa50c580559c0234ac
Dennys Rivera <00026919@uca.edu.sv>
2020-04-25 00:17:23 -0600

    Add: Lab 05 Exercise 03

---
+++ Labo 05/Ejercicio_03/Ejercicio_03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_03
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese el n máximo: ");
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            Recursion(n, 1);
+        }
+
+        public static void Recursion(int n, int i)
+        {
+            if (i == n)
+            {
+                Console.Write(i + " ");
+            }
+            else
+            {
+                Console.Write(i + " ");
+                Recursion(n, ++i);
+                Console.Write(--i + " ");
+            }
+        }
+    }
+}
