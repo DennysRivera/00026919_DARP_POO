2090d4d634ea233cb2592af00ddcd798bb71acc3
Dennys Rivera <00026919@uca.edu.sv>
2020-05-21 10:22:53 -0600

    Modify: Static class & RS methods

--- Labo 09/Ejercicio 01/RegisterStudent.cs
+++ Labo 09/Ejercicio 01/RegisterStudent.cs
@@ -9,6 +9,34 @@ namespace Ejercicio_01
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("") ||
+                textBox2.Text.Equals("") ||
+                textBox3.Text.Equals("") ||
+                textBox4.Text.Equals(""))
+            {
+                MessageBox.Show("No se pueden dejar campos vacíos");
+            }
+            else
+            {
+                try
+                {
+                    ConnectionDB.ExecuteNonQuery($"INSERT INTO ESTUDIANTE VALUES(" +
+                                                 $"'{textBox3.Text}'," +
+                                                 $"'{textBox1.Text}'," +
+                                                 $"'{textBox2.Text}'," +
+                                                 $"'{textBox4.Text}')");
+
+                    MessageBox.Show("Se ha registrado el estudiante");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ha ocurrido un error");
+                }
+            }
+        }
        
     }
 }
